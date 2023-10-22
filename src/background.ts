97ebaa3b9bff3a54781222e8be7b3e2ccd872626
Looyyd <filip.a.niedzielski@gmail.com>
2023-10-22 20:03:56 +0200

    default list created on installation

---
+++ src/background.ts
@@ -0,0 +1,12 @@
+import {createNewList} from "./helpers";
+
+//TODO: browser agnostic
+chrome.runtime.onInstalled.addListener(async function(details) {
+  if (details.reason === 'install') {
+    // This block will run when the extension is first installed
+    await createNewList('default');
+  } else if (details.reason === 'update') {
+    // This block will run when the extension is updated
+    // You can also set or update storage values here
+  }
+});
