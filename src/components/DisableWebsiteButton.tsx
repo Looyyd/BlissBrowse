89436e06b134394e3122ac591f3201051b2cf199
Looyyd <filip.a.niedzielski@gmail.com>
2023-10-27 19:09:00 +0200

    removed unused

--- src/components/DisableWebsiteButton.tsx
+++ src/components/DisableWebsiteButton.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import {
   BlacklistDatastore,
   currentTabHostname,
-  isHostnameDisabled,
 } from "../modules/hostname";
 import Button from "@mui/material/Button"
 
