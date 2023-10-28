ebb658c7f3142b9d9b386113baf78ddb056e5236
Looyyd <filip.a.niedzielski@gmail.com>
2023-10-28 21:29:04 +0200

    loading screen component

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,23 @@
+// LoadingComponent.js
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+const LoadingScreen = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw'
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default LoadingScreen;
+
