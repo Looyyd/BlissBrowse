a0db58d3c7524676009a3d6822e72c73b7a30e86
Looyyd <filip.a.niedzielski@gmail.com>
2023-10-29 19:29:04 +0100

    blacklisted sites uses LoadingScreen.tsx

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,4 +1,3 @@
-// LoadingComponent.js
 import React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
--- src/components/popup/ListsDisplay.tsx
+++ src/components/popup/ListsDisplay.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {List, ListItem, ListItemText, Typography} from '@mui/material';
 import {ListNamesDataStore} from "../../modules/wordLists";
+import LoadingScreen from "../LoadingScreen";
 
 
 
@@ -14,8 +15,7 @@ const ListsDisplay: React.FC = () => {
       <Typography variant="h6">Your Lists</Typography>
       <List id="listsList">
         {lists === null ? (
-          //TODO: proper loading screen
-          <Typography variant="body1">Loading...</Typography>
+          <LoadingScreen/>
         ) : (
           lists.map((listName, index) => (
           <div key={index}>
