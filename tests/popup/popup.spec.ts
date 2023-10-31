cc5d121ae1129a75e1b34517a2c09d9fdb96c2c0
Looyyd <filip.a.niedzielski@gmail.com>
2023-10-31 11:19:57 +0100

    helpers and refactoring for tests

--- tests/popup/popup.spec.ts
+++ tests/popup/popup.spec.ts
@@ -1,29 +1,5 @@
-import { testSpec, expect } from '../fixtures';
-import {BrowserContext} from "@playwright/test";
-
-async function addList(listName: string, page: any) {
-  // Enter a new list name to the input field
-  const newListName = listName;
-  const inputField = page.locator('#listNameInputField');
-
-  await inputField.fill(newListName);
-
-  // Click on the #newListSubmit button to submit the new list
-  await page.locator('#newListSubmit').click();
-
-  // Wait for potential animations, network requests, or DOM updates
-  await page.waitForTimeout(1000);  // Adjust this timeout as needed
-}
-
-async function waitNewTab(context: BrowserContext) {
-  // Wait for the new tab to open
-  const currentPages = context.pages();
-  await context.waitForEvent('page', { timeout: 5000 });
-  const allPages = context.pages();
-  const newTab = allPages.find(p => !currentPages.includes(p));
-  if (!newTab) throw new Error('No new tab found');
-  return newTab;
-}
+import {expect, testSpec} from '../fixtures';
+import {addListWithPopup, addWordWithPopup, getNewTabAfterAction, getNewTabPromise} from "../testHelpers";
 
 testSpec.describe('Popup Tests', () => {
   // The setup that will be done before each test in this describe block
@@ -35,16 +11,17 @@ testSpec.describe('Popup Tests', () => {
     await expect(page.locator('#openOptionsButton')).toHaveText('Open Options');
     const currentPages = context.pages();
     await page.waitForTimeout(500);//small delay because option button was breaking test
-    await page.locator('#openOptionsButton').click();
 
-    const newTab = await waitNewTab(context);
+    const optionButton = page.locator('#openOptionsButton');
+    const newTab = await getNewTabAfterAction(context, () => optionButton.click());
+
     const newPageURL = newTab.url();
     expect(newPageURL.endsWith('/options.html')).toBe(true);
   });
 
   testSpec('popup add list', async ({ page, extensionId, context }) => {
     const newListName = "list1";
-    await addList(newListName, page);
+    await addListWithPopup(newListName, page);
 
     // Check if listsList contains the added list
     const listsList = page.locator('#listsList');  // Assuming `#listsList` is the container for your lists
@@ -54,15 +31,15 @@ testSpec.describe('Popup Tests', () => {
 
   testSpec('popup edit list', async ({ page, extensionId, context }) => {
     const listName = "list1";
-    await addList(listName, page);
+    await addListWithPopup(listName, page);
 
     // Select the edit button of id "edit-" + listName
     const editButton = page.locator(`#edit-${listName}`);
 
     await expect(editButton).toBeVisible();
-    await editButton.click();
 
-    const newTab = await waitNewTab(context);
+    const newTab = await getNewTabAfterAction(context, () => editButton.click());
+
     // Switch context to the new tab
     await newTab.bringToFront();
 
@@ -73,30 +50,15 @@ testSpec.describe('Popup Tests', () => {
 
     testSpec('popup add word', async ({ page, extensionId, context }) => {
       const listName = "list1";
-      await addList(listName, page);
+      const wordToAdd = "SampleWord";
+      await addListWithPopup(listName, page);
 
       //create new word
-      const inputField = page.locator('#addWordTextInput');
-      await expect(inputField).toBeVisible();
-      const wordToAdd = "SampleWord";
-      await inputField.fill(wordToAdd);
-      //await page.locator('#customWordListSelect').selectOption(listName);
-      // Click the MUI select dropdown to open the options
-      await page.locator('#customWordListSelect').click();
+      await addWordWithPopup(page, listName, wordToAdd);
 
-      // Wait a moment for the dropdown animation (might need to adjust timeout based on your application's behavior)
-      await page.waitForTimeout(500);
+      const editButton = page.locator(`#edit-${listName}`);
+      const newTab = await getNewTabAfterAction(context, () => editButton.click());
 
-      // Choose an option from the dropdown by its text content. For example, to select "list1":
-      await page.locator('li[role="option"]:text("list1")').click();
-      //await page.locator('text="list1"').click();
-
-      await page.locator('#submitNewWordButton').click();
-      await page.waitForTimeout(1000);  // Adjust this timeout as needed
-
-      await page.locator(`#edit-${listName}`).click();
-
-      const newTab = await waitNewTab(context);
       // Switch context to the new tab
       await newTab.bringToFront();
 
@@ -104,7 +66,7 @@ testSpec.describe('Popup Tests', () => {
       //small delay because lists needs to load
       await newTab.waitForTimeout(500);
       const textAreaContent = await newTab.locator('#filterWordlistsEditorTextArea').inputValue();
-      console.log("textAreacContent", textAreaContent);
+      console.log("textAreaContent", textAreaContent);
       expect(textAreaContent.includes(wordToAdd)).toBe(true);
     });
 
---
+++ tests/testHelpers.ts
@@ -0,0 +1,63 @@
+import {BrowserContext, Page} from "@playwright/test";
+import {expect} from "./fixtures";
+
+export async function addListWithPopup(listName: string, page: any) {
+  // Enter a new list name to the input field
+  const newListName = listName;
+  const inputField = page.locator('#listNameInputField');
+
+  await inputField.fill(newListName);
+
+  // Click on the #newListSubmit button to submit the new list
+  await page.locator('#newListSubmit').click();
+
+  // Wait for potential animations, network requests, or DOM updates
+  await page.waitForTimeout(1000);  // Adjust this timeout as needed
+}
+
+export async function getNewTabPromise(context: BrowserContext): Promise<Page> {
+  // Get the current pages
+  const currentPages = context.pages();
+
+  // Create a promise that resolves when a new page is opened
+  const waitForNewPagePromise = context.waitForEvent('page', {timeout: 5000});
+
+  // Wait for the new page event to fire
+  await waitForNewPagePromise;
+
+  // Find the new page
+  const allPages = context.pages();
+  const newTab = allPages.find(p => !currentPages.includes(p));
+  if (!newTab) throw new Error('No new tab found');
+
+  return newTab;
+}
+
+export async function getNewTabAfterAction(context: BrowserContext, action: () => Promise<void>): Promise<Page> {
+    const newTabPromise = getNewTabPromise(context);
+    await action();
+    const newTab = await newTabPromise;
+    return newTab;
+}
+
+
+export async function addWordWithPopup(page: Page, listName: string, word: string) {
+        //create new word
+      const inputField = page.locator('#addWordTextInput');
+      await expect(inputField).toBeVisible();
+      const wordToAdd = "SampleWord";
+      await inputField.fill(wordToAdd);
+      //await page.locator('#customWordListSelect').selectOption(listName);
+      // Click the MUI select dropdown to open the options
+      await page.locator('#customWordListSelect').click();
+
+      // Wait a moment for the dropdown animation (might need to adjust timeout based on your application's behavior)
+      await page.waitForTimeout(500);
+
+      // Choose an option from the dropdown by its text content. For example, to select "list1":
+      await page.locator('li[role="option"]:text("list1")').click();
+      //await page.locator('text="list1"').click();
+
+      await page.locator('#submitNewWordButton').click();
+      await page.waitForTimeout(1000);  // Adjust this timeout as needed
+}
