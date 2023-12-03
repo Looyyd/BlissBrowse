5a5e210f9235d6f37c44cba7b9854e0c27494017
Looyyd <filip.a.niedzielski@gmail.com>
2023-12-03 12:50:00 +0100

    title in black always(it appeared white on tooltip)

--- src/components/ExtensionTitle.tsx
+++ src/components/ExtensionTitle.tsx
@@ -6,11 +6,12 @@ import {EXTENSION_NAME} from "../constants";
 const StyledTypography = styled(Typography)({
   textAlign: 'center',
   textDecoration: 'underline',
+  color: "black",//hardcoded black because it appeared white in tooltip
 });
 
 function ExtensionTitle() {
   return (
-    <StyledTypography variant="h5">
+    <StyledTypography  variant="h5">
       {EXTENSION_NAME} - Content Filter
     </StyledTypography>
   );
